27773db0f2cb95cda7eb1caf810d5d9b0d55f581
Ian Vanagas <34755028+ivanagas@users.noreply.github.com>
2023-02-19 14:35:21 -0800

    fission analytics packages (#4)

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Header from '@components/Header'
 import MobileNav from '@components/MobileNav'
 import Body from '@components/Body'
 import Sidebar from '@components/Sidebar'
+import Script from 'next/script'
 
 export default function Home() {
   return (
@@ -17,6 +18,19 @@ export default function Home() {
       </main>
       <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossOrigin="anonymous"></script>
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossOrigin="anonymous"></script>
+      <Script
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.async=!0,p.src=s.api_host+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="capture identify alias people.set people.set_once set_config register register_once unregister opt_out_capturing has_opted_out_capturing opt_in_capturing reset isFeatureEnabled onFeatureFlags".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
+            posthog.init(
+              'phc_gHVx9rtr4KFugRpW7S5oyJWJQka1TQaFLaXIEs7xMID',
+              {
+                api_host:'https://app.posthog.com'
+              }
+            )
+            `
+        }}
+      />
     </div>
   )
 }
